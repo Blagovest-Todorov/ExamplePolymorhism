290a14ad834a39225340150282620ca4265036c1
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-02 10:51:40 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _16.OopPolymorhismExample
+{
+    class Program  //  polymorphism allows us the reusability of the code! 
+    {
+        static void Main(string[] args)
+        {
+          
+            Animal myAnimal = new Animal();
+            Animal myPig = new Pig();
+            Animal myDog = new Dog();
+
+
+            myAnimal.AnimalSound();
+            myPig.AnimalSound();   //polymorphyism --child method replaces the parent method implementation.
+            myDog.AnimalSound();
+            
+
+        }
+    }
+    class Animal // Base Parent class
+    {
+        public virtual void AnimalSound()  //virtual keyword -access modifier 
+               // virtual-allow access and modify the parent method to be overriden into the child class
+        {
+            Console.WriteLine("The animal makes a sound! ");
+        }
+    }
+
+    class Dog : Animal //derived class
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("the dog is bow-wow ! ");
+        }
+    }
+
+    class Pig : Animal  // derived class
+    {
+        public override void AnimalSound() 
+        {
+            Console.WriteLine("The Pig is squeeking ! ");
+        }
+    }
+
+    
+
+}
