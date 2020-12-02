587c6c50e1de9adadabb4fec9280480537c36191
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-02 10:55:28 +0200

    correction

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace _16.OopPolymorhismExample
     {
         static void Main(string[] args)
         {
-          
+
             Animal myAnimal = new Animal();
             Animal myPig = new Pig();
             Animal myDog = new Dog();
@@ -15,14 +15,14 @@ namespace _16.OopPolymorhismExample
             myAnimal.AnimalSound();
             myPig.AnimalSound();   //polymorphyism --child method replaces the parent method implementation.
             myDog.AnimalSound();
-            
+
 
         }
     }
     class Animal // Base Parent class
     {
         public virtual void AnimalSound()  //virtual keyword -access modifier 
-               // virtual-allow access and modify the parent method to be overriden into the child class
+                                           // virtual-allows access and modifies the parent method to be overriden into the child class
         {
             Console.WriteLine("The animal makes a sound! ");
         }
@@ -30,7 +30,7 @@ namespace _16.OopPolymorhismExample
 
     class Dog : Animal //derived class
     {
-        public override void AnimalSound()
+        public override void AnimalSound()  // overiding method, replaces the method from the paren class
         {
             Console.WriteLine("the dog is bow-wow ! ");
         }
@@ -38,12 +38,12 @@ namespace _16.OopPolymorhismExample
 
     class Pig : Animal  // derived class
     {
-        public override void AnimalSound() 
+        public override void AnimalSound()
         {
             Console.WriteLine("The Pig is squeeking ! ");
         }
     }
 
-    
+
 
 }
